f259f8542bd75d101ea40e386fc43d07e9f9c028
Quang-tuoi-tre <honhatquang890@gmail.com>
2024-04-07 22:58:43 +0700

    Chao buoi toi

---
+++ MD5/src/main/java/com/mycompany/md5/MD5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.md5;
+
+/**
+ *
+ * @author honha
+ */
+public class MD5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
